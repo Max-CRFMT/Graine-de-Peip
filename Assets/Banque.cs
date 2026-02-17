eb4aee006e516a6b3afe0e34b14c3c17f3bc9878
PizzaRoyal <quentinlgm.maestre@free.fr>
2026-02-17 18:41:00 +0100

    Avancement sur la banque

--- Assets/Banque.cs
+++ Assets/Banque.cs
@@ -1,6 +1,45 @@
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Banque
+public class Banque : MonoBehaviour
 {
+    public Queue<Cartes> FileDeCartes = new Queue<Cartes>();
+    public void TestCode()
+    {
+        Cartes Fleur1 = new Cartes("1", true, "Vallée", 3);
+        Cartes Fleur2 = new Cartes("2", true, "Montagne", 5);
+        Cartes Fleur3 = new Cartes("3", true, "Plage", 1);
+        Cartes Fleur4 = new Cartes("4", true, "Gravier", 46);
+
+        //Debug.Log(Cartes.nombredecartes);
+
+        if (FileDeCartes.Count <= 3)
+        {
+            FileDeCartes.Enqueue(Fleur1);
+        }
+        else
+        {
+            FileDeCartes.Dequeue();
+        }
+        Debug.Log(FileDeCartes.Count);
+    }
     
+}
+
+public class Cartes
+{
+    public static int nombredecartes;
+    public Cartes(string name, bool boolstockable, string typebiome, int effectiftotal)
+    {
+        string nom = name;
+        bool stockable = boolstockable;
+        string biome = typebiome;
+        string PathImage = Application.persistentDataPath + "\assets ou un truc du genre" + nom + ".png";
+        int effectif_total = effectiftotal;
+        nombredecartes++;
+    }
 }
