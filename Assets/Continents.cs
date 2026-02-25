508c64ade1c9229cfde2d882cd0bcd8855a95800
NL-42 <lombardini.nicolas17@gmail.com>
2026-02-25 18:11:30 +0100

    kill me
    Crée le préfab Species Stack qui:
    - S'affiche sous condition
    - Est grisé si quantité <= 0
    - normal sinon

--- Assets/Continents.cs
+++ Assets/Continents.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Continent
 {
-    public List<PileEspece> SoclePileEspeces; //Socle contenant plusieurs piles d'espèces.
+    //public List<SpeciesStack> SocleSpeciesStacks; //Socle contenant plusieurs piles d'espèces.
     public List<string> ListSpeciesName; //Liste de string ayant le nom des espèces présentes dans le continent. Importé du ficheir excel.
 
     Dictionary<string, List<string>> Dict_Continent_Biomes = new Dictionary<string, List<string>>()
@@ -22,7 +22,7 @@ public class Continent
         List<string> biomes = Dict_Continent_Biomes[nom];
         int EducationLevel = 0;
         List<Carte> PileFaceCachee = PileCacheeEnFonctionDuContinent();
-        List<PileEspece> SoclePileEspeces = new List<PileEspece>(); //Socle contenant plusieurs piles d'espèces.
+        //List<SpeciesStack> SocleSpeciesStacks = new List<SpeciesStack>(); //Socle contenant plusieurs piles d'espèces.
     }
 
     public List<Carte> PileCacheeEnFonctionDuContinent()
@@ -34,9 +34,9 @@ public class Continent
     public void SocleMaker()
     {
         //Cette fonction a pour but de creer l'ensemble de liste de pile de graines en fonction du continent.
-        foreach (string SpeciesName in ListSpeciesName)
-        {
-            SoclePileEspeces.Add(new PileEspece(SpeciesName));
-        }
+        //foreach (string SpeciesName in ListSpeciesName)
+        //{
+        //    SocleSpeciesStacks.Add(new SpeciesStack(SpeciesName));
+        //}
     }
 }
