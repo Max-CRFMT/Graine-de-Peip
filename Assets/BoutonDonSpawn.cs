ebc6c0f31b28194623ef0890105e5f7c5b8523b9
PizzaRoyal <quentinlgm.maestre@free.fr>
2026-05-05 17:07:44 +0200

    Séance 5 mai
    Affichage des joueurs dans le don

--- Assets/BoutonDonSpawn.cs
+++ Assets/BoutonDonSpawn.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class BoutonDonSpawn : MonoBehaviour
 {
     public GameObject bouton;
     public Transform CanvasTransform;
     public RectTransform SpawnBoutonDON;
-    public int espace;
+    public int espace = 40;
     [ContextMenu("Apparaition")]
     public void AparaitionBouton()
     {
-        int nb_bouton = 5;
+        int nb_bouton = GameLogic.instance.Liste_Joueurs.Count;
         int temp = espace;
         for (int i = 0; i < nb_bouton; i++) 
         {
+            bouton.tag = "Joueur" + (i + 1).ToString();
             GameObject nouveauBouton = Instantiate(bouton, CanvasTransform);
+            GameObject.FindWithTag(bouton.tag);
+            TMP_Text nom_joueur = nouveauBouton.GetComponentInChildren<TMP_Text>();
+            nom_joueur.text = GameLogic.instance.Liste_Joueurs[i].pseudo;
             RectTransform rt = nouveauBouton.GetComponent<RectTransform>();
             rt.anchoredPosition = SpawnBoutonDON.anchoredPosition;
 
