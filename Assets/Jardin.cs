2f7c032ba388b8460791cc0b4b5ce5530d860a94
admin <corfmat.maxence@gmail.com>
2026-05-26 10:41:44 +0200

    Fix update jardin

--- Assets/Jardin.cs
+++ Assets/Jardin.cs
@@ -66,19 +66,16 @@ public class Jardin
 
     public void UpdateSpriteJardin()
     {
-        for (int i = 1; i != niveau_jardin+1; i++)
+        for (int i = 1; i != 9; i++)
         {
             string obj = "Jardin" + i.ToString();
-            Debug.Log(obj);
             if (Liste_Carte.Count() >= i)
             {
-                Debug.Log("oui");
                 GameObject.Find(obj).gameObject.GetComponent<GestionInteractionJardin>().carte_contenue = Liste_Carte[i - 1];
             } else
             {
                 GameObject.Find(obj).gameObject.GetComponent<GestionInteractionJardin>().carte_contenue = null;
             }
-            Debug.Log("non");
 
             GameObject.Find(obj).gameObject.GetComponent<GestionInteractionJardin>().MiseAJourUI();
             var go = GameObject.Find(obj).gameObject;
